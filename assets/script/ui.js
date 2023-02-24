8108fe0167b42903540e99030f9a725225efdab4
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-02-24 18:12:12 +0330

    Add ui code

---
+++ assets/script/ui.js
@@ -0,0 +1,9 @@
+//UI FOR WEATHER API
+class UI {
+    constructor() {
+        this.icon = document.getElementById('w-icon');
+        this.temp = document.getElementById('w-temp');
+    }
+}
+const config = new Config();
+const ui = new UI();
